4bfa9d94947e19345ccf800251b644e16393cd6d
Jureva <ejure@ymail.com>
2018-04-18 22:26:56 +0100

    validation began

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="hello">
     <div class="holder">
+      
+      <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill you got.." v-model="skill" v-validate="'min:5'" name="skill">
+      <p class="alert" v-if="errors.has('skill')">{{errors.first('skill') }}</p>
+     </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }} </li>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
-
-        <div v-bind:class="{ alert: showAlert }"></div>
+      <p>These are the fuckin awesome skills.</p>
       </div>
   </div>
 </template>
@@ -15,11 +19,23 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
      skills: [
        { "skill": "Vue.js"},
        { "skill": "Frontend Developer"}
-     ],
-     showAlert: true
+     ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) =>{
+        if (result) {
+          this.skills.push({skill:this.skill})
+          this.skill = '';
+        } else {
+          console.log('not valid');
+        }
+      })
     }
   }
 }
@@ -27,9 +43,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .alert {
-    background-color: yellow;
-    width:100%;
-    height: 30px;
+  .holder {
+    background: #fff; 
+  } 
+  ul {
+    margin: 0; 
+    padding: 0; 
+    list-style-type: none; 
+  } 
+  ul li {
+    padding: 20px; 
+    font-size: 1.3em; 
+    background-color: #E0EDF4; 
+    border-left: 5px solid #3EB3F6; 
+    margin-bottom: 2px; 
+    color: #3E5252; 
+  } 
+  p {
+    text-align:center; 
+    padding: 30px 0; 
+    color: gray; 
+  } 
+  .container {
+    box-shadow: 0px 0px 40px lightgray; 
+  } 
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 </style>
